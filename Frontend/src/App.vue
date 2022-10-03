492cb87454afe6be224c1a9486ca07822cb142ad
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2022-10-03 09:03:50 +0300

    Changes

---
+++ Frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <UserMessages />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import UserMessages from "./components/UserMessages.vue";
+</script>
---
+++ Frontend/src/components/UserMessages.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4 class="text-h4">Messages</h4>
+    <v-card>
+      <v-list
+        :items="Messages"
+        item-title="Messages"
+        item-value="Messages.idx"
+      ></v-list>
+      <v-icon icon="fas fa-plus" />
+    </v-card>
+  </div>
+</template>
+<script setup>
+const Messages = ["hi", "hello", "itsWorking"];
+</script>
